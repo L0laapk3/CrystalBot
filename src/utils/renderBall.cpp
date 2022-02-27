e6a242919eb76f31fe235158f7eca9cc92f25842
L0laapk3 <L0laapk3@gmail.com>
2022-02-27 22:27:32 +0100

    rewrite circle function

--- src/utils/renderBall.cpp
+++ src/utils/renderBall.cpp
@@ -9,14 +9,14 @@
 
 constexpr float M_PI = 3.14159265358979323846;
 
-void renderCircle(RLURenderer& renderer, const vec3& center, float radius, const vec3& axis, const size_t numPoints, rlbot::Color color) {
+void renderCircle(RLURenderer& renderer, const vec3& c, float radius, const vec3& axis, const size_t numPoints, rlbot::Color color) {
 	std::vector<vec3> points{ numPoints + 1 };
 	float angle = 0;
 	const vec3 orth1 = normalize(vec3{ 0, -axis[2], axis[1] });
 	const vec3 orth2 = normalize(cross(axis, orth1));
 	for (int i = 0; i < numPoints; i++) {
 		angle += 2 * M_PI / numPoints;
-		points[i] = center + radius * (cosf(angle) * orth1 + sinf(angle) * orth2);
+		points[i] = c + radius * (cosf(angle) * orth1 + sinf(angle) * orth2);
 	}
 	points.back() = points.front();
 	renderer.DrawPolyLine3D(color, points);
@@ -28,145 +28,139 @@ void renderBall(RLURenderer& renderer, const RLBotBM::Ball& ball, rlbot::Color c
 	constexpr int numCircles = 5;
 	constexpr float cornerSpace = .125f;
 
-	constexpr int sideCircles = numCircles / 2;
-	constexpr bool middleCircle = numCircles % 2;
-	constexpr float stepPerSideCircle = (1.f - cornerSpace) / (sideCircles + 1);
-	std::array<std::pair<float, float>, sideCircles + middleCircle> sinCos;
-	for (int i = 0; i < sinCos.size(); i++) {
-		float sin = (i + !middleCircle) * stepPerSideCircle;
-		sinCos[i] = { sin, sqrtf(1 - sin * sin) };
-	}
-
 	const auto ballRotator = quatToRLU(ball.orientation);
 	std::array axes { vec3{ 1, 0, 0 }, vec3{ 0, 1, 0 }, vec3{ 0, 0, 1 } };
 	for (auto& axis : axes)
 		axis = dot(ballRotator, axis) * ball.radius;
 
+	constexpr int sideCircles = numCircles / 2;
+	constexpr bool middleCircle = numCircles % 2;
+	constexpr int numArcPoints = sideCircles + middleCircle;
+	constexpr float stepPerSideCircle = (1.f - cornerSpace) / (sideCircles + 1);
+
+	typedef std::array<vec3, numArcPoints> ArcPoints;
+	struct AxisArcPoint {
+		vec3 sin;
+		ArcPoints cosCos;
+		ArcPoints cosSin;
+	};
+	typedef std::array<AxisArcPoint, axes.size()> AxesM;
+	std::array<AxesM, numArcPoints> allAxesM;
+
+	for (int i = 0; i < axes.size(); i++)
+		for (int j = 0; j < numArcPoints; j++) {
+			float sin = (j + !middleCircle) * stepPerSideCircle;
+			allAxesM[j][i].sin = sin * axes[i];
+			const vec3 cosAxis = sqrtf(1 - sin * sin) * axes[i];
+			for (int k = 0; k < numArcPoints; k++) {
+				float sin = (k + !middleCircle) * stepPerSideCircle;
+				allAxesM[j][i].cosSin[k] = sin * cosAxis;
+				allAxesM[j][i].cosCos[k] = sqrtf(1 - sin * sin) * cosAxis;
+			}
+		}
+
 	std::vector<vec3> points{ 1200 }; // todo: calculate
 	auto it = points.begin();
 
-
-	auto arc45start = [&](vec3 center, vec3 axisFrom, vec3 axisTo, int startAt) {
-		for (int k = startAt; k < sinCos.size(); k++) {
-			const auto& [sin, cos] = sinCos[k];
-			*it++ = center + cos * axisFrom + sin * axisTo;
-		}
+	auto cInv = [&](vec3 v, bool inv) {
+		return inv ? -v : v;
 	};
-	auto arc45end = [&](vec3 center, vec3 axisFrom, vec3 axisTo, int endAt) {
-		for (int k = 0; k < endAt; k++) {
-			const auto& [sin, cos] = sinCos[k];
-			*it++ = center + cos * axisFrom + sin * axisTo;
-		}
+	auto arcPoint = [&](const vec3& center, const vec3& cosComp, const vec3& sinComp, bool fc = false, bool fCos = false, bool fSin = false) {
+		*it++ = sphereCenter + cInv(center, fc) + cInv(cosComp, fCos) + cInv(sinComp, fSin);
 	};
-	auto arc45 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) { arc45start(center, axisFrom, axisTo, 0); };
-	auto arc45r = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-		for (auto rit = sinCos.crbegin(); rit != sinCos.crend() - middleCircle; ++rit) {
-			auto& [sin, cos] = *rit;
-			*it++ = center + sin * axisFrom + cos * axisTo;
-		}
+	auto arc45start = [&](int c, int f, int t, const AxesM& axes, int startAt, bool fc = false, bool ff = false, bool ft = false) {
+		for (int k = startAt; k < numArcPoints; k++)
+			arcPoint(axes[c].sin, axes[f].cosCos[k], axes[t].cosSin[k], fc, ff, ft);
 	};
-	auto arc90 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-		arc45(center, axisFrom, axisTo);
-		arc45r(center, axisFrom, axisTo);
+	auto arc45 = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		arc45start(c, f, t, axes, 0, fc, ff, ft);
 	};
-	auto arc180 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-		arc90(center, axisFrom, axisTo);
-		arc90(center, axisTo, -axisFrom);
+	auto arc45end = [&](int c, int f, int t, const AxesM& axes, int endAt, bool fc = false, bool ff = false, bool ft = false) {
+		for (int k = 0; k < endAt; k++)
+			arcPoint(axes[c].sin, axes[f].cosCos[k], axes[t].cosSin[k], fc, ff, ft);
 	};
-	auto arc270 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-		arc180(center, axisFrom, axisTo);
-		arc90(center, -axisFrom, -axisTo);
+	auto arc45r = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		for (auto k = numArcPoints - 1; k > 0; k--)
+			arcPoint(axes[c].sin, axes[t].cosCos[k], axes[f].cosSin[k], fc, ft, ff);
+	};
+	auto arc90 = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		arc45(c, f, t, axes, fc, ff, ft);
+		arc45r(c, f, t, axes, fc, ff, ft);
+	};
+	auto arc180 = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		arc90(c, f, t, axes, fc, ff, ft);
+		arc90(c, t, f, axes, fc, ft, !ff);
+	};
+	auto arc270 = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		arc180(c, f, t, axes, fc, ff, ft);
+		arc90(c, f, t, axes, fc, !ff, !ft);
 	}; 
-	// auto arc270 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-	// 	arc90(center, axisFrom, -axisTo);
-	// 	arc90(center, -axisTo, -axisFrom);
-	// 	arc90(center, -axisFrom, axisTo);
-	// };
-	auto arc360OffsetStart = [&](vec3 center, vec3 axisFrom, vec3 axisTo, int startAt) {
-		arc45start(center, axisFrom, axisTo, startAt);
-		arc45r(center, axisFrom, axisTo);
-		arc270(center, axisTo, -axisFrom);
-		arc45end(center, axisFrom, axisTo, startAt);
+	auto arc360OffsetStart = [&](int c, int f, int t, const AxesM& axes, int startAt, bool fc = false, bool ff = false, bool ft = false) {
+		arc45start(c, f, t, axes, startAt, fc, ff, ft);
+		arc45r(c, f, t, axes, fc, ff, ft);
+		arc270(c, t, f, axes, fc, ft, !ff);
+		arc45end(c, f, t, axes, fc, startAt, ff, ft);
 	};
-	auto arc360 = [&](vec3 center, vec3 axisFrom, vec3 axisTo) {
-		arc360OffsetStart(center, axisFrom, axisTo, 0);
+	auto arc360 = [&](int c, int f, int t, const AxesM& axes, bool fc = false, bool ff = false, bool ft = false) {
+		arc360OffsetStart(c, f, t, axes, 0, fc, ff, ft);
 	};
-	// auto arc360OffsetEnd = [&](vec3 center, vec3 axisFrom, vec3 axisTo, int endAt) {
+
+
+	for (int i = 0; i < numArcPoints; i++) {
+		const auto& axesM = allAxesM[i];
 		
-	// 	for (int k = endAt; k --> 0;) {
-	// 		const auto& [sin, cos] = sinCos[k];
-	// 		*it++ = center + cos * axisFrom - sin * axisTo;
-	// 	}
-	// 	arc270(center, axisFrom, axisTo);
-	// 	arc45(center, -axisTo, axisFrom);
-	// 	for (int k = sinCos.size(); k --> endAt;) {
-	// 		const auto& [sin, cos] = sinCos[k];
-	// 		*it++ = center + cos * axisFrom - sin * axisTo;
-	// 	}
-	// };
-
-
-	for (int i = 0; i < sinCos.size(); i++) {
-		const auto& [sin1, cos1] = sinCos[i];
-		const std::array sinAxes { sin1 * axes[0], sin1 * axes[1], sin1 * axes[2] };
-		const std::array cosAxes { cos1 * axes[0], cos1 * axes[1], cos1 * axes[2] };
-		
-
-		for (int dir = 1; dir >= -1; dir -= 2) {
-			for (int j = 0; j < sinCos.size(); j++) {
-				const auto& [sin2, cos2] = sinCos[j];
-				const auto sin2s = copysignf(sin2, dir), cos2s = copysignf(cos2, dir);
+		auto& firstPoint = *it;
+		for (int dir = 0; dir < 2; dir++) {
+			for (int j = 0; j < numArcPoints; j++) {
 				if ((dir == -1 || !middleCircle || i) && i == j) {
-					const auto circleCenter = sphereCenter + sin2s * axes[1];
-					const auto circleFrom = cos2s * axes[0], circleTo = cos2 * axes[2];
-					arc45start(circleCenter, circleFrom, circleTo, j);
-					arc45r(circleCenter, circleFrom, circleTo);
-					arc90(circleCenter, circleTo, -circleFrom);
-					
-					arc45end(circleCenter, -circleFrom, -circleTo, j);
-					if (dir == 1)
-						arc360OffsetStart(sphereCenter - sinAxes[2], -cosAxes[0], cosAxes[1], j);
-					arc45start(circleCenter, -circleFrom, -circleTo, j);
-					arc45r(circleCenter, -circleFrom, -circleTo);
-					arc90(circleCenter, -circleTo, circleFrom);
+					arc45start(1, 0, 2, axesM, j, dir, dir, false);
+					arc45r(1, 0, 2, axesM, dir, dir, false);
+					arc90(1, 2, 0, axesM, dir, false, !dir);
 
-					arc45end(circleCenter, circleFrom, circleTo, j);
+					arc45end(1, 0, 2, axesM, j, dir, !dir, true);
+					if (dir == 1)
+						arc360OffsetStart(2, 0, 1, axesM, j, true, false, true);
+					arc45start(1, 0, 2, axesM, j, dir, !dir, true);
+					arc45r(1, 0, 2, axesM, dir, !dir, true);
+					arc90(1, 2, 0, axesM, dir, true, dir);
+
+					arc45end(1, 0, 2, axesM, j, dir, dir, false);
 				}
 					
-				*it++ = sphereCenter + sinAxes[2] + cos2s * cosAxes[0] + sin2s * cosAxes[1];
+				arcPoint(axesM[2].sin, axesM[0].cosCos[j], axesM[1].cosSin[j], false, dir, dir);
 			}
-			for (int j = sinCos.size(); j --> 1;) {
-				const auto& [sin2, cos2] = sinCos[j];
-				const auto sin2s = copysignf(sin2, dir), cos2s = copysignf(cos2, dir);
+			for (int j = numArcPoints; j --> 1;) {
 				if (i == j) {
-					auto sIt = it;
-					arc360OffsetStart(sphereCenter + sin2s * axes[0],  cos2s * axes[1], cos2 * axes[2], j);
-
+					arc360OffsetStart(0, 1, 2, axesM, j, dir, dir, false);
 
 					// sin2 * axes0
 					// cos2 * cos3 * axes1
 					// cos2 * sin3 * axes2
-					// renderer.DrawLine3D(rlbot::Color::red, sphereCenter, *sIt);
+					// renderer.DrawLine3D(rlbot::Color::red, sphereCenter, firstPoint);
 				}
-				*it++ = sphereCenter + sinAxes[2] + sin2s * cosAxes[0] + cos2s * cosAxes[1];
-				// if (i == j)
-					// cos1 * sin2 * axes0
-					// cos1 * cos2 * axes1
-					// sin1 * axes2
-					// renderer.DrawLine3D(rlbot::Color::green, sphereCenter, *(it - 1));
+				arcPoint(axesM[2].sin, axesM[1].cosCos[j], axesM[0].cosSin[j], false, dir, dir);
+				// if (i == j && dir == 1)
+				// 	// cos1 * sin2 * axes0
+				// 	// cos1 * cos2 * axes1
+				// 	// sin1 * axes2
+				// 	renderer.DrawLine3D(rlbot::Color::green, sphereCenter, *(it - 1));
 			}
 			if (dir == 1 && middleCircle && !i)
-				arc360(sphereCenter, axes[1], axes[2]);
-			arc90(sphereCenter + sinAxes[2], dir * cosAxes[1], dir * -cosAxes[0]);
+				arc360(0, 1, 2, axesM, false, true, true);
+			arc90(2, 1, 0, axesM, false, dir, !dir);
 		}
 		
-		*it++ = sphereCenter + sinAxes[2] + cosAxes[0];
+		*it++ = firstPoint;
 
-		
-		// arc90(sphereCenter + sinAxes[2], cosAxes[0], cosAxes[1]);
-		// arc360(sphereCenter + sinAxes[0], cosAxes[1], cosAxes[2]);
-		// arc270(sphereCenter + sinAxes[2], cosAxes[1], cosAxes[0]);
-		// arc360(sphereCenter + sinAxes[1], cosAxes[0], cosAxes[2]);
+		// arc90(2, 0, 1, axesM);
+		// arc360(0, 1, 2, axesM);
+		// arc270(2, 1, 0, axesM, false, false, true);
+		// arc360(1, 0, 2, axesM);
+	}
+
+	if (middleCircle) {
+		arc45r(1, 0, 2, allAxesM[0]);
+		arc270(1, 2, 0, allAxesM[0], false, false, true);
 	}
 
 
