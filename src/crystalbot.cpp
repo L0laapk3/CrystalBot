858584420e2e04b61a302652753414b3f77db088
L0laapk3 <L0laapk3@gmail.com>
2023-01-14 20:47:35 +0100

    new version with new orbuculum

--- src/crystalbot.cpp
+++ src/crystalbot.cpp
@@ -54,75 +54,77 @@ ActionSequenceExecutor simSeqEx;
 bool seqInitialized = false;
 RLBotBM::ControllerInput CrystalBot::tick(RLBotBM::GameState& state) {
 
-	auto& stateSetObj = rlbot::bmInterface->getStateSetObj();
-	int dt = state.tick - lastTick;
-	lastTick = state.tick;
+	return RLBotBM::ControllerInput();
 
-	if (dt != 1)
-		std::cout << "skipped ticks in rlbot-bm kekw? dt=" << dt << " tick=" << state.tick << std::endl;
+	// auto& stateSetObj = rlbot::bmInterface->getStateSetObj();
+	// int dt = state.tick - lastTick;
+	// lastTick = state.tick;
 
-	if (seqInitialized)
-		seqInitialized = !seqEx.step(seq.end(), dt);
+	// if (dt != 1)
+	// 	std::cout << "skipped ticks in rlbot-bm kekw? dt=" << dt << " tick=" << state.tick << std::endl;
+
+	// if (seqInitialized)
+	// 	seqInitialized = !seqEx.step(seq.end(), dt);
 	
-    if (!seqInitialized) {
-		float steer = (float)rand() / RAND_MAX * 2 - 1;
-		int time = 300 * rand() / RAND_MAX;
-		std::cout << "sequence time: " << time << " steer: " << steer << std::endl;
+    // if (!seqInitialized) {
+	// 	float steer = (float)rand() / RAND_MAX * 2 - 1;
+	// 	int time = 300 * rand() / RAND_MAX;
+	// 	std::cout << "sequence time: " << time << " steer: " << steer << std::endl;
 
-		seq.clear();
-		seq.push_back({ 2,    { }}); // defines number of statesetting ticks
-		seq.push_back({ 250,  { .throttle = 1, .steer = -.1, .boost = 1 }});
-		seq.push_back({ 1,    { .throttle = 1, .jump = 1 }});
-		seq.push_back({ 10,   { .throttle = 1, .pitch = steer, .yaw = steer, .roll = steer, .jump = 1 }});	
-		seq.push_back({ time, { .throttle = 1, .pitch = steer, .yaw = steer, .roll = steer, .boost = 1 }});
-		seq.push_back({ 1,    { .throttle = 1, .steer = steer  }});	
+	// 	seq.clear();
+	// 	seq.push_back({ 2,    { }}); // defines number of statesetting ticks
+	// 	seq.push_back({ 250,  { .throttle = 1, .steer = -.1, .boost = 1 }});
+	// 	seq.push_back({ 1,    { .throttle = 1, .jump = 1 }});
+	// 	seq.push_back({ 10,   { .throttle = 1, .pitch = steer, .yaw = steer, .roll = steer, .jump = 1 }});	
+	// 	seq.push_back({ time, { .throttle = 1, .pitch = steer, .yaw = steer, .roll = steer, .boost = 1 }});
+	// 	seq.push_back({ 1,    { .throttle = 1, .steer = steer  }});	
 
-		seqEx.reset(seq.begin());
-		seqInitialized = true;
+	// 	seqEx.reset(seq.begin());
+	// 	seqInitialized = true;
 
-		// index = rand() > RAND_MAX / 2 ? 0 : 1;
-	}
+	// 	// index = rand() > RAND_MAX / 2 ? 0 : 1;
+	// }
 
-	if (seqEx.currentStep == seq.begin()) {
-		auto quat = quatFromRPY({ 0, 0, -1 });
-		if (statevar) {
-			stateSetObj.balls[0].position = { 1000, 0, 100 };
-			stateSetObj.balls[0].velocity = { 0, 0, 0 };
-			stateSetObj.balls[0].angularVelocity = { 100, 0, 0 };
-			stateSetObj.balls[0].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
-			stateSetObj.cars[index].boost = 100;
+	// if (seqEx.currentStep == seq.begin()) {
+	// 	auto quat = quatFromRPY({ 0, 0, -1 });
+	// 	if (statevar) {
+	// 		stateSetObj.balls[0].position = { 1000, 0, 100 };
+	// 		stateSetObj.balls[0].velocity = { 0, 0, 0 };
+	// 		stateSetObj.balls[0].angularVelocity = { 100, 0, 0 };
+	// 		stateSetObj.balls[0].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
+	// 		stateSetObj.cars[index].boost = 100;
 
-			std::cout << "setting car & ball" << std::endl;
+	// 		std::cout << "setting car & ball" << std::endl;
 			
-			stateSetObj.cars[index].position = { 0, 2500, 25.53 };
-			stateSetObj.cars[index].velocity = { 0, 0, 0 };
-			stateSetObj.cars[index].angularVelocity = { 0, 0, 0 };
-			stateSetObj.cars[index].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
-		} else {
-			if (!hasCapturedTick2) {
+	// 		stateSetObj.cars[index].position = { 0, 2500, 25.53 };
+	// 		stateSetObj.cars[index].velocity = { 0, 0, 0 };
+	// 		stateSetObj.cars[index].angularVelocity = { 0, 0, 0 };
+	// 		stateSetObj.cars[index].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
+	// 	} else {
+	// 		if (!hasCapturedTick2) {
 				
-			}
-			stateSetObj.cars[index].position = { 0, 2500, 25.53 };
-			stateSetObj.cars[index].velocity = { 0, 0, 0 };
-			stateSetObj.cars[index].angularVelocity = { 0, 0, 0 };
-			stateSetObj.cars[index].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
+	// 		}
+	// 		stateSetObj.cars[index].position = { 0, 2500, 25.53 };
+	// 		stateSetObj.cars[index].velocity = { 0, 0, 0 };
+	// 		stateSetObj.cars[index].angularVelocity = { 0, 0, 0 };
+	// 		stateSetObj.cars[index].orientation = reinterpret_cast<RLBotBM::StateSetQuat&>(quat);
 
-			std::cout << "setting car" << index << std::endl;
-		}
+	// 		std::cout << "setting car" << index << std::endl;
+	// 	}
 
-		stateSetObj.setAny = true;
+	// 	stateSetObj.setAny = true;
 
-		statevar = false;
-	}
+	// 	statevar = false;
+	// }
 
-	if (seqEx.currentStep == seq.begin() + 3 && !statevar) {
-		if (std::find(different_values.begin(), different_values.end(), game.ball.position[1]) == different_values.end())
-			different_values.push_back(game.ball.position[1]);
-		std::cout << std::setprecision(20) << "ball end position: " << game.ball.position << " # unique outcomes: " << different_values.size() << std::endl;
-		statevar = true;
-	}
+	// if (seqEx.currentStep == seq.begin() + 3 && !statevar) {
+	// 	if (std::find(different_values.begin(), different_values.end(), game.ball.position[1]) == different_values.end())
+	// 		different_values.push_back(game.ball.position[1]);
+	// 	std::cout << std::setprecision(20) << "ball end position: " << game.ball.position << " # unique outcomes: " << different_values.size() << std::endl;
+	// 	statevar = true;
+	// }
 
-    return seqEx.getInput();
+    // return seqEx.getInput();
 }
 
 
--- src/crystalbot.h
+++ src/crystalbot.h
@@ -4,10 +4,14 @@
 #include "rlbot/bot.h"
 #include "utils/ringBuffer.hpp"
 
+#include "Orbuculum.h"
+
 class CrystalBot : public rlbot::Bot {
     Game game;
 public:
-    CrystalBot(int _index, int _team, std::string _name);
+    CrystalBot(int index, int team, std::string name);
+
+	Orbuculum orb;
 
 	RingBuffer<RLBotBM::ControllerInput, 20> lastControls;
 
